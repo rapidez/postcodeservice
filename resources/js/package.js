ae598f2362fa972bbc18b12f47c50e6d9ee47bd9
indy koning <indy@justbetter.nl>
2024-02-27 15:46:27 +0100

    Listen on postcode-change event

--- resources/js/package.js
+++ resources/js/package.js
@@ -1,37 +1,55 @@
+import { set, useDebounceFn, useMemoize } from "@vueuse/core";
+
 document.addEventListener('turbo:load', function () {
+    window.app.$on('postcode-change', useDebounceFn(updateAddressFromPostcodeservice, 100));
+
     ['shipping_address', 'billing_address'].forEach((type) => {
         window.app.$watch('checkout.'+type+'.postcode', function () {
-            getAddressFromPostcodeservice(type)
+            window.app.$emit('postcode-change', window.app.checkout[type])
         })
 
         window.app.$watch('checkout.'+type+'.street.1', function () {
-            getAddressFromPostcodeservice(type)
+            window.app.$emit('postcode-change', window.app.checkout[type])
+        })
+
+        window.app.$watch('checkout.'+type+'.country_id', function () {
+            window.app.$emit('postcode-change', window.app.checkout[type])
         })
     })
 })
 
-async function getAddressFromPostcodeservice(type) {
-    if (window.app.checkout[type].country_id != 'NL') {
+const getAddressFromPostcodeservice = useMemoize(
+    async function (postcode, housenumber) {
+        return window.axios.post(window.url('/api/postcodeservice'), {
+            postcode: postcode,
+            housenumber: housenumber,
+        }, {
+            headers: {
+                accept: 'application/json',
+            }
+        })
+    }
+)
+
+async function updateAddressFromPostcodeservice(address) {
+    if ((address?.country_id || address?.country_code) != 'NL') {
         return
     }
 
-    let response = await window.axios.post(window.url('/api/postcodeservice'), {
-        postcode: window.app.checkout[type].postcode,
-        housenumber: window.app.checkout[type].street[1],
-    }, {
-        headers: {
-            accept: 'application/json',
-        }
-    })
+    if(!address.postcode || !(address?.housenumber || address.street[1])) {
+        return
+    }
+
+    let response = await getAddressFromPostcodeservice(address.postcode, address?.housenumber || address.street[1])
 
     if (!response.data?.city || !response.data?.street) {
         if (response.data?.error == "Postcode not found") {
-            window.app.checkout[type].city = ''
-            window.app.checkout[type].street[0] = ''
+            set(address, 'city', '')
+            set(address.street, 0, '')
         }
         return
     }
 
-    window.app.checkout[type].city = response.data.city
-    window.app.checkout[type].street[0] = response.data.street
+    set(address, 'city', response.data.city)
+    set(address.street, 0, response.data.street)
 }
